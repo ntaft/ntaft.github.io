e1f1a83e983f16a7845c86b7e05b07cc19364509
Nick Taft <taft82@gmail.com>
2016-10-24 16:58:19 -0400

    moving files around

--- profile/jscript.js
+++ profile/jscript.js
@@ -1,7 +1,11 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   function expandContent($clickedBox, hiddenContent) {
-  	$clickedBox.style.width = '46%';
+    if (window.innerWidth > 768){
+  	   $clickedBox.style.width = '46%';
+    } else {
+      $clickedBox.style.width = '98%';
+    }
     // $clickedBox.style.opacity = '1';
     //reveals the hidden content in each box; a timeout for this one matches css
   	setTimeout( () => {document.getElementById(hiddenContent).style.display='block';}, 500);
@@ -9,12 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   function resetWidth ($clickedBox) {
+    const winWid = window.innerWidth
   	for (let i = 1; i < 8; i++) {
   		let boxNumber = `box${i}`
       let $eachBox = document.getElementById(boxNumber);
       //makes sure that all are reset except for current item
   		if ($eachBox != $clickedBox) {
+        if (winWid > 768) {
   			$eachBox.style.width = '22%';
+      } else {
+        $eachBox.style.width = '48%';
+      }
   			document.getElementById(`hidden${i}`).style.display = 'none';
   		};
   	};
