7fcdcc8db747d67f5662cdf4a57e1975a62fc724
Nick Taft <taft82@gmail.com>
2017-01-09 11:00:02 -0500

    integrating GOL code with splash

--- public/js/gol.js
+++ public/js/gol.js
@@ -1,86 +1,19 @@
+// () => {
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  const header = document.getElementsByTagName('header');
-  header[0].style.height = `${window.innerHeight}px`;
-
-
-  function expandContent(clickedBox, hiddenContent) {
-    if (window.innerWidth > 768){
-  	   clickedBox.style.width = '46%';
-    } else {
-      clickedBox.style.width = '98%';
-    }
-    // clickedBox.style.opacity = '1';
-    //reveals the hidden content in each box; a timeout for this one matches css
-  	setTimeout( () => {document.getElementById(hiddenContent).style.display='block';}, 500);
-  	resetWidth(clickedBox);
-  };
-
-  function resetWidth (clickedBox) {
-    const winWid = window.innerWidth
-  	for (let i = 1; i < 7; i++) {
-  		let boxNumber = `box${i}`
-      let eachBox = document.getElementById(boxNumber);
-      //makes sure that all are reset except for current item
-  		if (eachBox != clickedBox) {
-        if (winWid > 768) {
-  			eachBox.style.width = '33%';
-      } else {
-        eachBox.style.width = '99%';
-      }
-      document.getElementById(`hidden${i}`).style.display = 'none';
-      };
-  	}
-  }
-
-  for (let i = 1; i < 7; i++) {
-    let box = document.getElementById(`box${i}`);
-    box.addEventListener('click', () => expandContent(box, `hidden${i}`));
-  };
-
-  function buttonHandler() {
-  const button = document.querySelector('button');
-  button.onclick = sendMail
-}
-
-  function sendMail (e) {
-  e.preventDefault();
-
-  const mailPayload = {
-    name: e.path[1][0].value,
-    email: e.path[1][1].value,
-    body: e.path[1][2].value,
-    }
-  console.log(mailPayload);
-  for (i=0; i < e.path[1].length; i++) {
-    e.path[1][i].value = "";
-  }
-  const button = document.querySelector('button');
-  button.innerHTML = "Thanks for contacting me!";
-  button.removeEventListener("click", sendMail)
-  }
-
-  buttonHandler();
-
-  // <-------------- game of life code ---------------->
-
-  const canvas = document.querySelector('.board');
-  const context = canvas.getContext('2d');
-  canvas.style.height = `${window.innerHeight}px`;
-  canvas.style.width = `${window.innerHeight}px`;
-
+  console.log('loaded and ready to go');
 
   var board = [];
   var game = {
     rows: 50,
     columns: 50,
-    speed: 300,
+    speed: 100,
     density: 1.2,
-    pxWide: canvas.clientWidth,
-    pxHigh: canvas.clientHeight
+    pxWide: document.querySelector('.board').style.width,
+    pxHigh: document.querySelector('.board').style.height
   };
 
+const canvas = document.querySelector('.board');
+  const context = canvas.getContext('2d');
 
 
   // // pixel ratio adjuster via https://github.com/jondavidjohn/hidpi-canvas-polyfill
@@ -246,7 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // canvas.style.width = `${window.innerWidth}px`
 
   function drawDot(x, y, radius, color) {
-    context.fillStyle = color;
+        context.fillStyle = color;
     context.fillRect(x, y, radius*2, radius*2)
     // context.beginPath();
     // context.arc(x, y, radius, 0, 2 * Math.PI, false);
@@ -256,4 +189,6 @@ document.addEventListener('DOMContentLoaded', () => {
  initBoard();
  var gameOfLife = setInterval(lifeGen, game.speed);
 
-});
+//end of the DOM load script
+// };
+
