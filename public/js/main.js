95297c520454ece60b89d66655659fccf5e6be14
Nick Taft <taft82@gmail.com>
2017-01-11 14:09:55 -0500

    updated proj links

--- public/js/main.js
+++ public/js/main.js
@@ -5,39 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
   header[0].style.height = `${window.innerHeight}px`;
 
 
-  function expandContent(clickedBox, hiddenContent) {
-    if (window.innerWidth > 768){
-  	   clickedBox.style.width = '46%';
-    } else {
-      clickedBox.style.width = '98%';
-    }
-    // clickedBox.style.opacity = '1';
-    //reveals the hidden content in each box; a timeout for this one matches css
-  	setTimeout( () => {document.getElementById(hiddenContent).style.display='block';}, 500);
-  	resetWidth(clickedBox);
-  };
+  // function expandContent(clickedBox, hiddenContent) {
+  //   if (window.innerWidth > 768){
+  // 	   clickedBox.style.width = '46%';
+  //   } else {
+  //     clickedBox.style.width = '98%';
+  //   }
+  //   // clickedBox.style.opacity = '1';
+  //   //reveals the hidden content in each box; a timeout for this one matches css
+  // 	setTimeout( () => {document.getElementById(hiddenContent).style.display='block';}, 500);
+  // 	resetWidth(clickedBox);
+  // };
 
-  function resetWidth (clickedBox) {
-    const winWid = window.innerWidth
-  	for (let i = 1; i < 7; i++) {
-  		let boxNumber = `box${i}`
-      let eachBox = document.getElementById(boxNumber);
-      //makes sure that all are reset except for current item
-  		if (eachBox != clickedBox) {
-        if (winWid > 768) {
-  			eachBox.style.width = '33%';
-      } else {
-        eachBox.style.width = '99%';
-      }
-      document.getElementById(`hidden${i}`).style.display = 'none';
-      };
-  	}
-  }
+  // function resetWidth (clickedBox) {
+  //   const winWid = window.innerWidth
+  // 	for (let i = 1; i < 7; i++) {
+  // 		let boxNumber = `box${i}`
+  //     let eachBox = document.getElementById(boxNumber);
+  //     //makes sure that all are reset except for current item
+  // 		if (eachBox != clickedBox) {
+  //       if (winWid > 768) {
+  // 			eachBox.style.width = '33%';
+  //     } else {
+  //       eachBox.style.width = '99%';
+  //     }
+  //     document.getElementById(`hidden${i}`).style.display = 'none';
+  //     };
+  // 	}
+  // }
 
-  for (let i = 1; i < 7; i++) {
-    let box = document.getElementById(`box${i}`);
-    box.addEventListener('click', () => expandContent(box, `hidden${i}`));
-  };
+  // for (let i = 1; i < 7; i++) {
+  //   let box = document.getElementById(`box${i}`);
+  //   box.addEventListener('click', () => expandContent(box, `hidden${i}`));
+  // };
 
   function buttonHandler() {
   const button = document.querySelector('button');
@@ -65,22 +65,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // <-------------- game of life code ---------------->
 
+  // sets up canvas variables; sets width to window
   const canvas = document.querySelector('.board');
   const context = canvas.getContext('2d');
   canvas.height = `${window.innerWidth}`;
   canvas.width = `${window.innerWidth}`;
   context.globalAlpha=0.8;
 
-
+  // sets up board variables
   var board = [];
   var game = {
     rows: 50,
     columns: 50,
-    speed: 200,
+    speed: 300,
     density: 1.2,
     pxWide: canvas.width,
     pxHigh: canvas.height,
-    color: '#f44242'
+    color1: 'rgb(255, 0, 0)',
+    color2: 'rgb(255, 0, 0)'
   };
 
   // // pixel ratio adjuster via https://github.com/jondavidjohn/hidpi-canvas-polyfill
@@ -102,7 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const canvasWidth = canvas.width // / getPixelRatio(context);
   const canvasHeight = canvas.height // / getPixelRatio(context);
 
-//   board.offscreenCanvas = document.createElement("canvas");
+// board.offscreenCanvas = document.createElement("canvas");
 // board.offscreenCanvas.width = canvasHeight;
 // board.offscreenCanvas.height = canvasWidth;
 // board.offscreenContext = board.offscreenCanvas.getContext("2d");
@@ -190,7 +192,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
     function lifeGen() {
-      context.clearRect( 0, 0, canvasWidth, canvasHeight)
+      context.fillStyle="rgb(240, 240, 240)";
+      context.fillRect( 0, 0, canvasWidth, canvasHeight)
       x = 0;
       y = -1;
       yBound = 0;
@@ -213,11 +216,11 @@ document.addEventListener('DOMContentLoaded', () => {
           // If the cell is alive, then it stays alive only if it has  2 or 3 live neighbors.
           if ((cellVal === 1) && (( neighbors <= 3 ) && (neighbors >= 2))) {
            state = 1;
-           drawDot(xPos, yPos, dotRadius, game.color);
+           drawDot(xPos, yPos, dotRadius, game.color1);
          // If the cell is dead, then it becomes alive only if it has 3 live neighbors.
          } else if ((cellVal === 0) && (neighbors === 3)) {
            state = 1;
-            drawDot(xPos, yPos, dotRadius, game.color);
+            drawDot(xPos, yPos, dotRadius, game.color2);
          } else {
            state = 0;
            // context.clearRect( xPos, yPos, dotRadius*2, dotRadius*2);
@@ -246,7 +249,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function drawDot(x, y, radius, color) {
     context.fillStyle = color;
-    context.fillRect(x, y, radius*2, radius*2)
+    context.fillRect(x, y, radius*2, radius*2);
     // context.beginPath();
     // context.arc(x, y, radius, 0, 2 * Math.PI, false);
     // context.fill();
