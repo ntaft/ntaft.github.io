bb7df66293d3e9b2e08cfeb9accb536942b8de05
Nick Taft <taft82@gmail.com>
2017-01-12 13:27:53 -0500

    updated icons

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
