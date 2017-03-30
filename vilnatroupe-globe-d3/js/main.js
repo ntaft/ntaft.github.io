a94cc1f0cbd9f428441d3f4d8fc00d43a7f49739
Nick Taft <taft82@gmail.com>
2017-03-29 22:41:08 -0400

    synced with repository

--- vilnatroupe-globe-d3/js/main.js
+++ vilnatroupe-globe-d3/js/main.js
@@ -1,210 +1,299 @@
-// // code adapted from http://bl.ocks.org/dwtkns/4973620
 
-d3.select(window)
-    .on("mousemove", mousemove)
-    .on("mouseup", mouseup);
+document.addEventListener('DOMContentLoaded', () => {
 
-var width = 960,
-    height = 500;
+// d3 code adapted from http://bl.ocks.org/dwtkns/4973620
+  d3.select(window)
+    .on('mousemove', mousemove)
+    .on('mouseup', mouseup);
 
-var proj = d3.geo.orthographic()
+  let width = 960;
+  let height = 960;
+
+  let proj = d3.geo.orthographic()
     .translate([width / 2, height / 2])
     .clipAngle(90)
-    .scale(220);
+    .scale(220 * 2);
 
-var sky = d3.geo.orthographic()
+  let sky = d3.geo.orthographic()
     .translate([width / 2, height / 2])
     .clipAngle(90)
-    .scale(260);
+    .scale(280 * 2);
 
-var path = d3.geo.path().projection(proj).pointRadius(2);
+  let path = d3.geo.path().projection(proj).pointRadius(2);
 
-var swoosh = d3.svg.line()
-      .x(function(d) { return d[0] })
-      .y(function(d) { return d[1] })
-      .interpolate("cardinal")
-      .tension(0);
+  let swoosh = d3.svg.line()
+    .x(d => d[0])
+    .y(d => d[1])
+    .interpolate('cardinal')
+    .tension(0);
 
-var links = [],
-    arcLines = [];
+  let links = [],
+      arcLines = [];
 
-var svg = d3.select("body").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .on("mousedown", mousedown);
+  let svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .on('mousedown', mousedown);
 
-queue()
-    .defer(d3.json, "https://d3js.org/world-110m.v1.json")
-    .defer(d3.json, "http://www.nicktaft.com/vilna/vt-geo-v2.json")
+  queue()
+    .defer(d3.json, '/data/vt-geo-v2')
+    .defer(d3.json, '/data/world-110m.v1')
     .await(ready);
 
-function ready(error, world, places) {
-  var ocean_fill = svg.append("defs").append("radialGradient")
-        .attr("id", "ocean_fill")
-        .attr("cx", "75%")
-        .attr("cy", "25%");
-      ocean_fill.append("stop").attr("offset", "5%").attr("stop-color", "rgb(200, 200, 240)");
-      ocean_fill.append("stop").attr("offset", "100%").attr("stop-color", "rgb(150, 150, 170)");
+  const color = d3.scale.ordinal().range(["#48A36D",  "#56AE7C",  "#64B98C", "#72C39B", "#80CEAA", "#80CCB3", "#7FC9BD", "#7FC7C6", "#7EC4CF", "#7FBBCF", "#7FB1CF", "#80A8CE", "#809ECE", "#8897CE", "#8F90CD", "#9788CD", "#9E81CC", "#AA81C5", "#B681BE", "#C280B7", "#CE80B0", "#D3779F", "#D76D8F", "#DC647E", "#E05A6D", "#E16167", "#E26962", "#E2705C", "#E37756", "#E38457", "#E39158", "#E29D58", "#E2AA59", "#E0B15B", "#DFB95C", "#DDC05E", "#DBC75F", "#E3CF6D", "#EAD67C", "#F2DE8A"]);
 
-  var globe_highlight = svg.append("defs").append("radialGradient")
-        .attr("id", "globe_highlight")
-        .attr("cx", "75%")
-        .attr("cy", "25%");
-      globe_highlight.append("stop")
-        .attr("offset", "5%").attr("stop-color", "rgb(200, 240, 200)")
-        .attr("stop-opacity","0.6");
-      globe_highlight.append("stop")
-        .attr("offset", "100%").attr("stop-color", "rgb(100, 120, 100)")
-        .attr("stop-opacity","0.2");
+  function ready(error, world, places) {
+    let troupeList = Array.from(new Set(places.features.map(feat => feat.properties.troupe)));
+    console.log(troupeList);
+    color.domain(troupeList.filter(function(key) { // Set the domain of the color ordinal scale to be filtered by troupe; needs work.
+      return key;
+    }));
+    let ocean_fill = svg.append('defs')
+      .append('radialGradient')
+      .attr('id', 'ocean_fill')
+      .attr('cx', '75%')
+      .attr('cy', '25%');
+    ocean_fill.append('stop')
+      .attr('offset', '5%')
+      .attr('stop-color', 'rgb(200, 200, 240)');
+    ocean_fill.append('stop')
+      .attr('offset', '100%')
+      .attr('stop-color', 'rgb(150, 150, 170)');
 
-  var globe_shading = svg.append("defs").append("radialGradient")
-        .attr("id", "globe_shading")
-        .attr("cx", "55%")
-        .attr("cy", "45%");
-      globe_shading.append("stop")
-        .attr("offset","30%").attr("stop-color", "#fff")
-        .attr("stop-opacity","0")
-      globe_shading.append("stop")
-        .attr("offset","100%").attr("stop-color", "#505962")
-        .attr("stop-opacity","0.3")
+    let globe_highlight = svg.append('defs')
+      .append('radialGradient')
+      .attr('id', 'globe_highlight')
+      .attr('cx', '75%')
+      .attr('cy', '25%');
+    globe_highlight.append('stop')
+      .attr('offset', '5%')
+      .attr('stop-color', 'rgb(200, 240, 200)')
+      .attr('stop-opacity','0.6');
+    globe_highlight.append('stop')
+      .attr('offset', '100%')
+      .attr('stop-color', 'rgb(100, 120, 100)')
+      .attr('stop-opacity','0.2');
 
-  svg.append("circle")
-    .attr("cx", width / 2).attr("cy", height / 2)
-    .attr("r", proj.scale())
-    .attr("class", "noclicks")
-    .style("fill", "url(#ocean_fill)");
+    let globe_shading = svg.append('defs')
+      .append('radialGradient')
+      .attr('id', 'globe_shading')
+      .attr('cx', '55%')
+      .attr('cy', '45%');
+    globe_shading.append('stop')
+      .attr('offset','30%')
+      .attr('stop-color', '#fff')
+      .attr('stop-opacity','0')
+    globe_shading.append('stop')
+      .attr('offset','100%')
+      .attr('stop-color', '#505962')
+      .attr('stop-opacity','0.3')
 
-  svg.append("path")
-    .datum(topojson.feature(world, world.objects.land))
-    .attr("class", "land noclicks")
-    .attr("d", path);
+    svg.append('circle')
+      .attr('cx', width / 2)
+      .attr('cy', height / 2)
+      .attr('r', proj.scale())
+      .attr('class', 'noclicks')
+      .style('fill', 'url(#ocean_fill)');
 
-  svg.append("circle")
-    .attr("cx", width / 2).attr("cy", height / 2)
-    .attr("r", proj.scale())
-    .attr("class","noclicks")
-    .style("fill", "url(#globe_highlight)");
+    svg.append('path')
+      .datum(topojson.feature(world, world.objects.land))
+      .attr('class', 'land noclicks')
+      .attr('d', path);
 
-  svg.append("circle")
-    .attr("cx", width / 2).attr("cy", height / 2)
-    .attr("r", proj.scale())
-    .attr("class","noclicks")
-    .style("fill", "url(#globe_shading)");
+    svg.append('circle')
+      .attr('cx', width / 2).attr('cy', height / 2)
+      .attr('r', proj.scale())
+      .attr('class','noclicks')
+      .style('fill', 'url(#globe_highlight)');
 
-  svg.append("g").attr("class","points")
-      .selectAll("text").data(places.features)
-    .enter().append("path")
-      .attr("class", "point")
-      .attr("d", path);
+    svg.append('circle')
+      .attr('cx', width / 2).attr('cy', height / 2)
+      .attr('r', proj.scale())
+      .attr('class','noclicks')
+      .style('fill', 'url(#globe_shading)');
 
-  // spawn links between locations as source/target coord pairs
-  for (let i = 1; i < places.features.length; i++) {
-    links.push({
-      source: places.features[i-1].geometry.coordinates,
-      target: places.features[i].geometry.coordinates
+    svg.append('g').attr('class','points')
+      .selectAll('text').data(places.features)
+      .enter().append('path')
+      .attr('class', 'point')
+      .attr('d', path);
+
+    // spawn links between cities as source/target coord pairs
+    for (let i = 1; i < places.features.length - 1; i++) {
+      if (places.features[i-1].properties.troupe === places.features[i].properties.troupe) {
+        links.push({
+          source: places.features[i-1].geometry.coordinates,
+          target: places.features[i].geometry.coordinates,
+          srcProperties: places.features[i].properties,
+          trgProperties: places.features[i-1].geometry.properties,
+        });
+      }
+    }
+
+    // build geoJSON features from links array
+    links.forEach(function(e,i,a) {
+      let feature = {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': [e.source, e.target]
+        },
+        'properties': {
+          'source': e.srcProperties,
+          'target': e.trgProperties
+        }
+      };
+      arcLines.push(feature);
     });
-  }
 
-  // build geoJSON features from links array
-  links.forEach(function(e,i,a) {
-    var feature =   { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [e.source,e.target] }}
-    arcLines.push(feature)
-  })
+    // gets a list of all the troupe data; removes duplicates
+    // http://www.jstips.co/en/javascript/deduplicate-an-array/
 
-  svg.append("g").attr("class","arcs")
-    .selectAll("path").data(arcLines)
-    .enter().append("path")
-      .attr("class","arc")
-      .attr("d",path)
 
-  svg.append("g").attr("class","flyers")
-    .selectAll("path").data(links)
-    .enter().append("path")
-    .attr("class","flyer")
-    .attr("d", function(d) { return swoosh(flying_arc(d)) })
+    // maps colors to each troupe; ### placeholder colors
+    // const colors = ['red', 'blue', 'green', 'yellow', 'brown', 'orange', 'purple', 'cyan'];
+    // let count = -1;
+    // troupeList = troupeList)).map((troupe) => {
+    //   count++;
+    //   return [troupe, colors[count]];
+    // }
 
-  refresh();
-}
+    // via http://bl.ocks.org/DStruths/9c042e3a6b66048b5bd4
+    // code not yet stable
+    /*
+    const categories = color.domain().map(function(name) { // Nest the data into an array of objects with new keys
+      return {
+        name: name, // "name": the csv headers except date
+        values: places.features.map(function(d) { // maps each troupe to a color
+          return {
+            date: d.date,
+            troupe: +(d[troupe]),
+            };
+        }),
+        visible: true // all are visible by default
+      };
+    });
+    */
 
-function flying_arc(pts) {
-  var source = pts.source,
-      target = pts.target;
 
-// bends the arc trajectory in relation to the starting point
-  var mid = location_along_arc(source, target, 0.7);
-  var result = [ proj(source),
-                 sky(mid),
-                 proj(target) ]
-  return result;
-}
+      // svg.selectAll('.flyer')
+      // .data()
+      // .filter(':nth-child(even)')
+      // .style('stroke', 'darkblue');
 
-function refresh() {
-  svg.selectAll(".land").attr("d", path);
-  svg.selectAll(".point").attr("d", path);
+    svg.append('g').attr('class','arcs')
+      .selectAll('path').data(arcLines)
+      .enter().append('path')
+        .attr('class','arc')
+        .attr('d', path);
 
-  svg.selectAll(".arc").attr("d", path)
-    .attr("opacity", function(d) {
-        return fade_at_edge(d)
-    })
+    svg.append('g').attr('class','flyers')
+      .selectAll('path').data(links)
+      .enter().append('path')
+      .attr('class','flyer')
+      .attr("id", d => d.srcProperties.troupe) // Give each line ID of troupe identifier
+      .attr('d', function(d) { return swoosh(flying_arc(d)) });
 
-  svg.selectAll(".flyer")
-    .attr("d", function(d) { return swoosh(flying_arc(d)) })
-    .attr("opacity", function(d) {
-      return fade_at_edge(d)
-    })
+      /*
+      // example for color coded lines, need to adapt for
+  svg.append("path")
+      .attr("class", "line")
+      .style("pointer-events", "none") // Stop line interfering with cursor
+      .attr("id", function(d) {
+        return "line-" + d.name.replace(" ", "").replace("/", ""); // Give line id of line-(insert name, with any spaces replaced with no spaces)
+      })
+      .attr("d", function(d) {
+        return d.visible ? line(d.values) : null; // If array key "visible" = true then draw line, if not then don't
+      })
+      .attr("clip-path", "url(#clip)")//use clip path to make irrelevant part invisible
+      .style("stroke", function(d) { return color(d.name); });
+      */
 
-}
-
-function fade_at_edge(d) {
-  var centerPos = proj.invert([width/2,height/2]),
-      arc = d3.geo.greatArc(),
-      start, end;
-  // function is called on 2 different data structures..
-  if (d.source) {
-    start = d.source,
-    end = d.target;
-  }
-  else {
-    start = d.geometry.coordinates[0];
-    end = d.geometry.coordinates[1];
-  }
-
-  var start_dist = 1.57 - arc.distance({source: start, target: centerPos}),
-      end_dist = 1.57 - arc.distance({source: end, target: centerPos});
-
-  var fade = d3.scale.linear().domain([-.1,0]).range([0,.1])
-  var dist = start_dist < end_dist ? start_dist : end_dist;
-
-  return fade(dist)
-}
-
-function location_along_arc(start, end, loc) {
-  var interpolator = d3.geo.interpolate(start,end);
-  return interpolator(loc)
-}
-
-// modified from http://bl.ocks.org/1392560
-var m0, o0;
-function mousedown() {
-  m0 = [d3.event.pageX, d3.event.pageY];
-  o0 = proj.rotate();
-  d3.event.preventDefault();
-}
-function mousemove() {
-  if (m0) {
-    var m1 = [d3.event.pageX, d3.event.pageY]
-      , o1 = [o0[0] + (m1[0] - m0[0]) / 6, o0[1] + (m0[1] - m1[1]) / 6];
-    o1[1] = o1[1] > 30  ? 30  :
-            o1[1] < -30 ? -30 :
-            o1[1];
-    proj.rotate(o1);
-    sky.rotate(o1);
     refresh();
   }
-}
-function mouseup() {
-  if (m0) {
-    mousemove();
-    m0 = null;
+
+  function flying_arc(pts) {
+    let source = pts.source,
+        target = pts.target;
+
+  // bends the arc trajectory in relation to the starting point
+    let mid = location_along_arc(source, target, 0.7);
+    let result = [ proj(source),
+                   sky(mid),
+                   proj(target) ]
+    return result;
   }
-}
+
+  function refresh() {
+    svg.selectAll('.land').attr('d', path);
+    svg.selectAll('.point').attr('d', path);
+
+    svg.selectAll('.arc').attr('d', path)
+      .attr('opacity', function(d) {
+        return fade_at_edge(d)
+      })
+
+    svg.selectAll('.flyer')
+      .attr('d', function(d) { return swoosh(flying_arc(d)) })
+      .attr('opacity', function(d) {
+        return fade_at_edge(d)
+      })
+
+  }
+
+  function fade_at_edge(d) {
+    const centerPos = proj.invert([width/2,height/2]);
+    const arc = d3.geo.greatArc();
+    let start,
+        end;
+    // function is called on 2 different data structures..
+    if (d.source) {
+      start = d.source,
+      end = d.target;
+    }
+    else {
+      start = d.geometry.coordinates[0];
+      end = d.geometry.coordinates[1];
+    }
+
+    let start_dist = 1.57 - arc.distance({source: start, target: centerPos});
+    let end_dist = 1.57 - arc.distance({source: end, target: centerPos});
+
+    let fade = d3.scale.linear().domain([-.1,0]).range([0,.1])
+    let dist = start_dist < end_dist ? start_dist : end_dist;
+
+    return fade(dist)
+  }
+
+  function location_along_arc(start, end, loc) {
+    let interpolator = d3.geo.interpolate(start,end);
+    return interpolator(loc)
+  }
+
+  // modified from http://bl.ocks.org/1392560
+  let m0, o0;
+  function mousedown() {
+    m0 = [d3.event.pageX, d3.event.pageY];
+    o0 = proj.rotate();
+    d3.event.preventDefault();
+  }
+  function mousemove() {
+    if (m0) {
+      let m1 = [d3.event.pageX, d3.event.pageY]
+        , o1 = [o0[0] + (m1[0] - m0[0]) / 6, o0[1] + (m0[1] - m1[1]) / 6];
+      o1[1] = o1[1] > 30  ? 30  :
+              o1[1] < -30 ? -30 :
+              o1[1];
+      proj.rotate(o1);
+      sky.rotate(o1);
+      refresh();
+    }
+  }
+  function mouseup() {
+    if (m0) {
+      mousemove();
+      m0 = null;
+    }
+  }
+// end of DOM load
+});
