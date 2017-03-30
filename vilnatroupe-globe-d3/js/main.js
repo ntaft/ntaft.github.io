81d148ac740893f601c2ae6a8cb42a6fafdd576f
Nick Taft <taft82@gmail.com>
2017-03-29 23:06:55 -0400

    updated from repository

--- vilnatroupe-globe-d3/js/main.js
+++ vilnatroupe-globe-d3/js/main.js
@@ -43,11 +43,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const color = d3.scale.ordinal().range(["#48A36D",  "#56AE7C",  "#64B98C", "#72C39B", "#80CEAA", "#80CCB3", "#7FC9BD", "#7FC7C6", "#7EC4CF", "#7FBBCF", "#7FB1CF", "#80A8CE", "#809ECE", "#8897CE", "#8F90CD", "#9788CD", "#9E81CC", "#AA81C5", "#B681BE", "#C280B7", "#CE80B0", "#D3779F", "#D76D8F", "#DC647E", "#E05A6D", "#E16167", "#E26962", "#E2705C", "#E37756", "#E38457", "#E39158", "#E29D58", "#E2AA59", "#E0B15B", "#DFB95C", "#DDC05E", "#DBC75F", "#E3CF6D", "#EAD67C", "#F2DE8A"]);
 
   function ready(error, world, places) {
-    let troupeList = Array.from(new Set(places.features.map(feat => feat.properties.troupe)));
-    console.log(troupeList);
-    color.domain(troupeList.filter(function(key) { // Set the domain of the color ordinal scale to be filtered by troupe; needs work.
-      return key;
-    }));
+    // let troupeList = Array.from(new Set(places.features.map(feat => feat.properties.troupe)));
+    // console.log(troupeList);
+    // color.domain(troupeList.filter(function(key) { // Set the domain of the color ordinal scale to be filtered by troupe; needs work.
+    //   return key;
+    // }));
     let ocean_fill = svg.append('defs')
       .append('radialGradient')
       .attr('id', 'ocean_fill')
